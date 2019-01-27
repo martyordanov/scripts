e9e15578d9c0a2a6f86fcd2b47e962934f43d97e
Martin Yordanov <martin.r.yordanov@gmail.com>
2019-01-28 00:16:15 +0100

    Now it should work just fine

--- change_audio_device.sh
+++ change_audio_device.sh
@@ -8,13 +8,34 @@ dev3='alsa_output.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00.analog-
 
 case $1 in 
     bluetooth_speaker)
+        #this lets us interact with that device using standar controls (vol up
+        # vol down, mute...)
         pacmd set-default-sink $dev1
+        
+        #since the sink indecies varie like crazy (each application has its own
+        #index, and further more the index changes every time you move it) we
+        #need to get a list of indecies and loop trough them        
+        sinks=$(pacmd list-sink-inputs | grep index: | grep -o "[0-9]*")
+        for sink in $sinks
+        do
+            pacmd move-sink-input $sink $dev1
+        done
         ;;
     hdmi)
         pacmd set-default-sink $dev2
+        sinks=$(pacmd list-sink-inputs | grep index: | grep -o "[0-9]*")
+        for sink in $sinks
+        do
+            pacmd move-sink-input $sink $dev2
+        done
         ;;
     headphones)
         pacmd set-default-sink $dev3
+        sinks=$(pacmd list-sink-inputs | grep index: | grep -o "[0-9]*")
+        for sink in $sinks
+        do
+            pacmd move-sink-input $sink $dev3
+        done
         ;;
     *)
         echo $"Usage: $0 {bluetooth_speaker|hdmi|headphones}"
